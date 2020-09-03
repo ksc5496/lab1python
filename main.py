e472f81ad0ebfeadc77df0b934d787788fb81bb2
ksc5496 <ksc5496@psu.edu>
2020-09-03 18:14:36 +0000

    added float while coding for calculations.

---
+++ main.py
@@ -0,0 +1,10 @@
+temp1 = input("Enter temperature: ")
+unit = input("Enter unit in F/f or C/c: ")
+if unit == "F" or unit== "f":
+  temp2 = (float(temp1)-32)*(5/9)
+  print(str(temp1) + "° in Fahrenheit is equivalent to " + str(temp2) + "° Celsius.")
+elif unit == "C" or unit == "c":
+  temp3 = ((float(temp1)*9/5)+32)
+  print(str(temp1) + "° in Celsius is equivalent to " + str(temp3) + "° in Fahrenheit.")
+else:
+  print(f"Invalid unit({unit}).")
