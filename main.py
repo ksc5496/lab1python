f9612ed43655fc1938550cd801efab804e2cc768
ksc5496 <ksc5496@psu.edu>
2020-09-04 14:51:53 +0000

    used printf function.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-temp1 = input("Enter temperature: ")
+# Author: Krish Choudhary ksc5496@psu.edu
+# Collaborator: Nicole Giron nqg5259@psu.edu
+# Collaborator: Youhyun Kim yhk5109@psu.edu
+# Collaborator: Geng Niu gjn5124@psu.edu
+
+temp = input("Enter temperature: ")
 unit = input("Enter unit in F/f or C/c: ")
-if unit == "F" or unit== "f":
-  temp2 = (float(temp1)-32)*(5/9)
-  print(str(temp1) + "° in Fahrenheit is equivalent to " + str(temp2) + "° Celsius.")
+if unit == "F" or unit == "f":
+  temp = float(temp)
+  temp1 = (temp-32)*(5/9)
+  print(f'%s° in Fahrenheit is equal to %s° Celsius.'%(temp,temp1))
 elif unit == "C" or unit == "c":
-  temp3 = ((float(temp1)*9/5)+32)
-  print(str(temp1) + "° in Celsius is equivalent to " + str(temp3) + "° Fahrenheit.")
+  temp = float(temp)
+  temp2 = (9/5*temp)+32
+  print(f'%s° in Celsius is equal to %s° Fahrenheit.'%(temp,temp2))
 else:
-  print(f"Invalid unit({unit}).")
+  print(f'Invalid unit(%s).'%(unit))
